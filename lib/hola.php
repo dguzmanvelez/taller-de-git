68efa75b5552da4fa6737dc9935617e6b3275aba
Doming Guzman Velez <dguzmanvelez@iessansebastian.com>
2021-05-15 22:28:57 +0200

    hola usa la clase HolaMundo

--- lib/hola.php
+++ lib/hola.php
@@ -1,5 +1,13 @@
 <?php
 // Autor: Sergio Gómez <sergio@uco.es>
+<<<<<<< HEAD
 print "Introduce tu nombre:";
 $nombre = trim(fgets(STDIN));
 @print "Hola, {$nombre}\n";
+=======
+// El nombre por defecto es Mundo
+require('HolaMundo.php');
+
+$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+print new HolaMundo($nombre);
+>>>>>>> aabe7df (hola usa la clase HolaMundo)
